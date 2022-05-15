49c7b05015c123ceee5dded0520ec72d2a0c4c60
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:21:55 -0300

    conversao

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int meuInteiro = 10;
+            double meuDouble =  5.25;
+            bool meuBool = true;
+
+            Console.WriteLine(Convert.ToString(meuInteiro));
+            Console.WriteLine(Convert.ToDouble(meuInteiro));
+            Console.WriteLine(Convert.ToInt32(meuDouble));
+        }
+    }
+}
